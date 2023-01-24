ec4551a8903d30ce9f551a056b73ca2c44f23d0b
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-01-24 11:12:50 -0500

    Primer formulario

--- src/FormularioUno.java
+++ src/FormularioUno.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class FormularioUno {
+import javax.swing.*;
+
+public class FormularioUno {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JLabel Titutlo;
+    private JButton button1;
+    private JButton button2;
+    private JPanel Formulario1;
+
+    public static void main(String[] args) {
+        JFrame frame1 = new JFrame("Formulario 1");
+        frame1.setContentPane(new FormularioUno().Formulario1);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
+        frame1.pack();
+    }
 }
