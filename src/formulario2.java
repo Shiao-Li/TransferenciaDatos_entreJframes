b1850bb98955069496a6c1e744a4a3b89cbf1d48
Erick <erick.santillan@epn.edu.ec>
2023-01-24 11:31:47 -0500

    Commit Erick Santillan

---
+++ src/formulario2.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class formulario2 {
+
+    private JTextField textField2;
+    private JTextField textField1;
+    private JButton irAlFormulario1Button;
+    private JButton abrirFormulario3Button;
+    private JPanel panel2;
+
+    public static void main(String[] arg){
+        JFrame frame = new JFrame("x1");
+        frame.setContentPane(new formulario2().panel2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
